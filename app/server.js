d5f51dfc3a09de0b1b0155b38057d0d56f6df004
Pradyumnyadav0992 <94163028+Pradyumnyadav0992@users.noreply.github.com>
2025-08-01 12:02:00 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const os = require('os');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end(`Hello from Kubernetes 11:55 !\nPod Name: ${os.hostname()}`);
+  res.end(`Hello from Kubernetes 11:577 !\nPod Name: ${os.hostame()}`);
 });
 
 server.listen(3000, () => {
